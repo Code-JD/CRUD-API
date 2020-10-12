3d98308a642879d6f0e65f1d653f5950fb4e0c86
Jonathan Herring <jonathan.d.herring@gmail.com>
2020-10-12 14:30:52 -0700

    more examples in Tuples.py

--- Tuples.py
+++ Tuples.py
@@ -1,5 +1,16 @@
-t = "a", "b", "c"
-print(t)
+# t = "a", "b", "c"
+# print(t)
 
-print("a", "b", "c")
-print(("a", "b", "c"))
+# print("a", "b", "c")
+# print(("a", "b", "c"))
+
+weclome = "Welcome to my Nightmare", "Alice Cooper", 1975
+bad = "Bad Company" "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+emilda = "More Mayhem", "Emilda May", 2011
+metallica = "Ride the Lighting", "Metallica", 1984
+
+print(metallica)
+print(metallica[0])
+print(metallica[1])
+print(metallica[2])
