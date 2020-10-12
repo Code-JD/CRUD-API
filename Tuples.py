26ad47a93ac289db2c6c297e3fdc924a8481ee8d
Jonathan Herring <jonathan.d.herring@gmail.com>
2020-10-12 14:36:53 -0700

    updated Tuples.py

--- Tuples.py
+++ Tuples.py
@@ -7,10 +7,20 @@
 weclome = "Welcome to my Nightmare", "Alice Cooper", 1975
 bad = "Bad Company" "Bad Company", 1974
 budgie = "Nightflight", "Budgie", 1981
-emilda = "More Mayhem", "Emilda May", 2011
+imelda = "More Mayhem", "Emilda May", 2011
 metallica = "Ride the Lighting", "Metallica", 1984
 
 print(metallica)
 print(metallica[0])
 print(metallica[1])
 print(metallica[2])
+
+# metallica[0] = "Master of Puppets"
+
+imelda = imelda[0], "Imelda May", imelda[2]
+print(imelda)
+
+metallica2 = ["Ride the Lightning", "Metallica", 1984]
+print(metallica2)
+metallica2[0] = "Master of Puppets"
+print(metallica2)
