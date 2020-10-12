5995c62673ae2163935efdd3c44538eebc708906
Jonathan Herring <jonathan.d.herring@gmail.com>
2020-10-12 14:47:34 -0700

    unpacked Tuple

--- Tuples.py
+++ Tuples.py
@@ -10,17 +10,10 @@ budgie = "Nightflight", "Budgie", 1981
 imelda = "More Mayhem", "Emilda May", 2011
 metallica = "Ride the Lighting", "Metallica", 1984
 
-print(metallica)
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
-
-# metallica[0] = "Master of Puppets"
-
-imelda = imelda[0], "Imelda May", imelda[2]
-print(imelda)
-
 metallica2 = ["Ride the Lightning", "Metallica", 1984]
 print(metallica2)
-metallica2[0] = "Master of Puppets"
-print(metallica2)
+
+title, artist, year = imelda
+print(title)
+print(artist)
+print(year)
